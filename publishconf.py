4472df40773c940e9cabfe716c1e74c3f8cb3c09
Matt Thomson <mattjohnthomson@gmail.com>
2017-01-10 20:13:50 +0000

    Serve over HTTPS

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://pyembed.github.io'
+SITEURL = 'https://pyembed.github.io'
 RELATIVE_URLS = False
 
 URL_ENDING = ''
